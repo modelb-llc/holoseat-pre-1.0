6191b984d60e110dc92a1eb48960bf3aefecb8da
brimstar <brimstar@20a06ef4-f98f-11e2-846c-0d8f3918c517>
2014-07-05 00:02:07 +0000

    Initial upload for holoseat configuration app.

---
+++ Holoseat Configuration App/trunk/HoloseatConfiguration/Configuration.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+
+namespace HoloSeatConfig
+{
+    public partial class Configuration : Form
+
+    {
+        private HoloseatConfigurationFile HoloConfig = new HoloseatConfigurationFile(); 
+        public Configuration()
+        {
+            InitializeComponent();
+            WalkKey.Text = HoloConfig.WalkCommandChar.ToString();
+            TriggerSteps.Value = Convert.ToInt16(HoloConfig.TriggerStepsPerMin);
+            TriggerStepsNumber.Text = TriggerSteps.Value.ToString();
+            int x = 1;
+
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                SerialPortList.Items.Add(s);
+                if (x==1)  
+                    SerialPortList.SelectedItem = s;
+
+            }
+            if (HoloConfig.SerialPort != "COM0")
+            {
+                SerialPortList.SelectedValue = HoloConfig.SerialPort;
+            }
+            else
+            {
+                HoloConfig.SerialPort = SerialPortList.SelectedItem.ToString();
+            }
+
+
+        }
+
+        private void TriggerSteps_Scroll(object sender, EventArgs e)
+        {
+            TriggerStepsNumber.Text = TriggerSteps.Value.ToString();
+        }
+
+
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            Result.Text = "";
+            UploadResult.Text = "";
+            int UpdateResult;
+            HoloConfig.TriggerStepsPerMin = TriggerSteps.Value;
+            HoloConfig.WalkCommandChar = Convert.ToChar(WalkKey.Text);
+            HoloConfig.SerialPort = SerialPortList.SelectedItem.ToString();
+            Result.Text = HoloConfig.WriteConfiguration();
+            UpdateResult = HoloConfig.UpdateHoloseat();
+            switch (UpdateResult)
+            {
+                case 0:
+                    UploadResult.Text = "Success";
+                    break;
+                default:
+                    UploadResult.Text = "Failed to upload(Error code:" + UpdateResult.ToString() + ").";
+                    break;
+            }
+        }
+
+        private void SaveClose_Click(object sender, EventArgs e)
+        {
+            Result.Text = "";
+            UploadResult.Text = "";
+            int UpdateResult;
+            HoloConfig.TriggerStepsPerMin = TriggerSteps.Value;
+            HoloConfig.WalkCommandChar = Convert.ToChar(WalkKey.Text);
+            HoloConfig.SerialPort = SerialPortList.SelectedItem.ToString();
+            string SaveResult;
+            SaveResult = HoloConfig.WriteConfiguration();
+            UpdateResult = HoloConfig.UpdateHoloseat();
+            switch (UpdateResult)
+            {
+                case 0:
+                    UploadResult.Text="Success";
+                    break;
+                default:
+                    UploadResult.Text="Failed to upload(Error code:"+ UpdateResult.ToString()+").";
+                    break;
+            }
+            if (SaveResult=="Success" & UploadResult.Text=="Success")
+                ActiveForm.Close();
+            else
+                Result.Text = SaveResult;
+
+        }
+
+        private void WalkKey_TextChanged(object sender, EventArgs e)
+        {
+            if (WalkKey.TextLength>1)
+                WalkKey.Text=WalkKey.Text.Substring(1,1);
+
+        }
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            ActiveForm.Close();
+        }
+
+
+
+ 
+
+
+
+
+
+    }
+}
---
+++ Holoseat Configuration App/trunk/HoloseatConfiguration/holoseat_firmware/holoseat_constants.h
@@ -0,0 +1,5 @@
+#ifndef holoseat_constants_h
+#define holoseat_constants_h
+const float TriggerStepsPerMin = 75;
+const char WalkCommandChar = 'W';
+#endif
