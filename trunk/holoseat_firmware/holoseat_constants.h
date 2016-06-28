ed860adca3c7c5ccdcdf285ab6383eb4b4599208
jrs <jrs@20a06ef4-f98f-11e2-846c-0d8f3918c517>
2016-06-28 04:12:09 +0000

    added first draft of HoloSeat Serial Protocol (HSP); see https://opendesignengine.net/projects/holoseat/wiki/Firmware_Notes for details; note, there are some critical FIXMEs, including a wait to start until a serial connection is made

--- trunk/holoseat_firmware/holoseat_constants.h
+++ trunk/holoseat_firmware/holoseat_constants.h
@@ -1,7 +1,14 @@
 #ifndef holoseat_constants_h
 #define holoseat_constants_h
 
-const float TriggerStepsPerMin = 75;
-const char WalkCommandChar = 'w';
+// default parameter values for Holoseat
+const char DefaultWalkCharacter = 'w';  // What key is sent to move the character in the game
+const unsigned int DefaultHoloseatEnabled = 1; // Is the Holoseat enabled by default?
+const unsigned int DefaultTriggerCadence = 75; // How fast does the user need to pedal (in RPM) to trigger walking?
+const unsigned int DefaultLoggingEnabled = 0; // Is serial logging enabled by default?
+const unsigned int DefaultLoggingInterval = 10; // How long between messages in serial logging in deci-seconds (0.1 of a second)
+
+// other boot parameters
+const unsigned int SerialBaudRate = 57600;
 
 #endif
