0c7276af1d6a5fcf252c85975b709134b8677694
J. Simmons, Ph.D <70734096+j-simmons-phd@users.noreply.github.com>
2016-11-29 01:42:59 +0000

    Added copyright, author, and updated license notices per PBI BIZ-45 (moving from attribution only to share alike style licensing).

--- test_rig/cad_models/rotating_arm.py
+++ test_rig/cad_models/rotating_arm.py
@@ -1,3 +1,15 @@
+# Copyright (C) 2016 Model B, LLC.
+# Designer: J. Simmons
+#
+# This documentation describes Open Hardware and is licensed under the CERN OHL v. 1.2.
+#
+# You may redistribute and modify this documentation under the terms of the CERN OHL v.1.2. 
+# (http://ohwr.org/cernohl). This documentation is distributed WITHOUT ANY EXPRESS OR IMPLIED 
+# WARRANTY, INCLUDING OF MERCHANTABILITY, SATISFACTORY QUALITY AND FITNESS FOR A PARTICULAR 
+# PURPOSE. Please see the CERN OHL v.1.2 for applicable conditions.
+#
+# Documentation location: https://opendesignengine.net/projects/holoseat
+
 # This example is meant to be used from within the CadQuery module of FreeCAD.
 import cadquery
 import math
