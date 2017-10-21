78119bffd72c9af016a5f244fb74f531d36a6a90
jrs <jrs@20a06ef4-f98f-11e2-846c-0d8f3918c517>
2017-10-21 22:53:49 +0000

    Got Nav Bar working and updated some Bootstrap defaults.

--- trunk/app-src/app/ui/__init__.py
+++ trunk/app-src/app/ui/__init__.py
@@ -7,12 +7,17 @@ import threading
 # decalre the UI object before importing other ui modules since they need UI
 UI = Flask(__name__)
 
+@UI.context_processor
+def inject_debug():
+    return dict(debug=UI.debug)
+
 # import required modules from the ui package so we can just run it
 from ui import views
 
 # turned off reloader per https://stackoverflow.com/a/24618018
+# set host to 0.0.0.0 per https://stackoverflow.com/a/7027113
 def uiThreadFunc(debug=False, port=8000):
-    UI.run(debug=debug, port=port, threaded=True, use_reloader=False)
+    UI.run(debug=debug, host= '0.0.0.0', port=port, threaded=True, use_reloader=False)
 
 # this is the function to use for starting the UI thread
 def start(debug=False, port=8000):
